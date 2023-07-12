8e474db17863c1fe9f633241f6bf9317e74a95de
tuandat-hcmus <ngotuandat.lop9a@gmail.com>
2023-07-12 08:07:47 +0000

    add project about-me

---
+++ README.md
@@ -0,0 +1,9 @@
+# LAB - 02
+
+## About Me
+
+I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+
+### Author: Ngo Tuan Dat
+
+### Links and resources
---
+++ js/app.js
@@ -0,0 +1,29 @@
+let userName = prompt("Please enter your name: ");
+alert("Welcome " + userName + " to the site. Please answer the following questions.");
+let rightAns = "Great!";
+let wrongAns = "Wrong answer!";
+
+function YesNoQuest(quest, ans){
+    let answer = prompt(quest);
+    answer = answer.toLowerCase();
+    if(answer === 'yes' || answer === 'y'){
+        answer = 'y';
+    }
+    else if(answer === 'no' || answer === 'n'){
+        answer = 'n';
+    }
+    if(answer === ans){
+        alert(rightAns);
+    }
+    else{
+        alert(wrongAns);
+    }
+}
+
+YesNoQuest("Am I over 20 years old?", 'n');
+YesNoQuest("Do I come from Dak Lak?", 'y');
+YesNoQuest("Do I know anything about C++?", 'y');
+YesNoQuest("Is  my major Computer Science?", 'n');
+YesNoQuest("Do I love football?", 'y');
+
+alert("Thank you for joining, " + userName);
